d5a81d0210da68e25505367407c1c819ccc30522
DESKTOP-GQ681HR\guyar <guy@experitest.com>
2018-07-11 16:34:11 +0300

    demo

--- spec/demo.js
+++ spec/demo.js
@@ -1,23 +1,30 @@
 var selenium = require('selenium-webdriver');
-var visual = require('galen-visual')
+var visual = require('galen-visual');
+
 let driver;
 describe('github navigation', function(){
-    beforeEach(function(done){
-        this.driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
-        this.driver.get("https://github.com").then(done);
-        visual.init('tR19OYaA4iRSW8s1vZm0LeB');
+    this.timeout(60000);
+    beforeEach( async function(){
+        driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
+        visual.init('PVVMjWyRpt5hiBJEkJW4aM2');
     });
-    afterEach(function(done) {
-        this.driver.quit().then(done);
-    });
-    it('should be on the home page', function(done){
-        this.driver.findElement(selenium.By.xpath('//*[@href="/features"]')).click();
-        visual.verify(this.driver, "github_home").then(function(result){
-            if(!result){
-                console.log(visual.getLastResultUrl());
-            }
-        });
-        console.log(result);
 
+    it('should be on the login page', async function(){
+        await driver.get("https://github.com/login");
+        //await driver.findElement(selenium.By.xpath("//a[contains(text(),'Sign in')]")).click();
+        var result = await visual.verify(driver, 'github_login');
+        console.log(result);
+        console.log(visual.getLastResultUrl());
+        console.log(visual.getLastSummary());
+
+    });
+
+    /*it('should be on the home page', async function(){
+        await driver.get("https://github.com");
+
+    });*/
+
+    afterEach( async function() {
+        await driver.quit();
     });
 })
